2096b68f5e7aac867206a989a90357386c83157e
howen02 <wenhyap02@gmail.com>
2024-07-13 20:22:31 +0800

    Change metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Focuslah",
+    title: "NUS Focus Areas",
     description: "NUS Computer Science focus areas at a glance",
 };
 
